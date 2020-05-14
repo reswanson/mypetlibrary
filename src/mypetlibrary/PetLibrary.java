a6882bd7e78b7625fb9bf00ea8581d732eb0fa97
reswanson <swansonr3@csp.edu>
2020-05-14 18:03:10 -0500

    Got adding working for single value, and changed display so it prints the new arraylist

--- src/mypetlibrary/PetLibrary.java
+++ src/mypetlibrary/PetLibrary.java
@@ -10,14 +10,13 @@ public class PetLibrary {
 	
 	PetLibrary() {
 	boolean exitnow = false;
-	//String[] pets = {};
-	//String[] pets = {"Fido", "mangy", "dog", "meowmeow"};
-    String[][] pets = { {"Kitty", "8"}, {"Bruno", "7"} };
     
-    //ArrayList<String><String> pets = new ArrayList<String><String>();
-    //pets.add({"Kitty", "8"});
-    //pets.add({"Bruno", "7"});
-
+    final ArrayList<String[]> pets = new ArrayList<String[]>();
+    
+    // TODO 
+    // Seeding the list, have to remember to remove before submitting
+	pets.add(new String[] {"kitty", "8"});
+	pets.add(new String[] {"bruno", "7"});
 
     
 	do {
@@ -36,7 +35,7 @@ public class PetLibrary {
 		    break;
 		  case "2":
 		    System.out.println("add");
-		    addPets();
+		    addPets(pets);
 		    break;
 		  case "3":
 		    System.out.println("update");
@@ -74,8 +73,8 @@ public class PetLibrary {
     }
     
     
-    public final void displayPets(String[][] mypets) {
-    	int numberofpets = mypets.length;
+    public final void displayPets(ArrayList<String[]> mypets) {
+    	//int numberofpets = mypets.length;
     	String name = "";
     	String age = "";
     	
@@ -85,78 +84,50 @@ public class PetLibrary {
 			
 			
 		// TODO 
-		// There is a better way to do this, but this is what I have
-		// so far,  can make it better in a future release.
-	    for (int i = 0; i < mypets.length; ++i) {
-	        name=mypets[i][0];
-	        age=mypets[i][1];
+		// There is a better way to do this, but this is what I have so far,  
+		// can make it better in a future release.	
+		int numberofpets = mypets.size();
+        for( int i = 0; i < numberofpets; i++ ) {
+	        name=mypets.get(i)[0];
+	        age=mypets.get(i)[1];
 	        System.out.printf("|%3.3s | %-12s | %3.3s |\n", i, name, age);
-	    }
-	       	   	
+        }
 
 		System.out.println("+-------------------------+");
 		System.out.println(numberofpets + " rows in set\n");
     }
     
-    public final void addPets() {
+    
+    /* addPets, allows user to add pet name and age to arraylist */
+    public final void addPets(ArrayList<String[]> mypets) {
     	
     	System.out.println("\n\n");
     	System.out.println("add pet (name age):");
         Scanner sc = new Scanner(System.in); 
         
-        // String input 
+        // get new pet from user 
         String mynewpet = sc.nextLine();
-        // new to chop of mynewpet to get name and age as separate values
+        
+        // chop up mynewpet to get name and age as separate values
         // split off of space char.
+        // TODO,  add validation logic to make user enter data in correct format.
         String[] arrayofpetdata = mynewpet.split(" ", 2);
         
         
         final String name=arrayofpetdata[0];
         final String age=arrayofpetdata[1];
-        System.out.printf("name: " + name + " age: " + age);
+        // System.out.printf("name: " + name + " age: " + age);
 
-        
-        
-        
-        final ArrayList<String[]> localpets = new ArrayList<String[]>();
-        //action.add(new String[2]);
-
-    	//ArrayList<String>[] localpets = new ArrayList<String>();
-    	
-    	String[] arr1 = { "bubba", "8" };
-    	String[] arr2 = { "babba", "7" };
-    	
-/*        aListArrays.add(new String[]{"element1"});
-        aListArrays.add(new String[]{"element1", "element2"});
-        aListArrays.add(new String[]{"element1", "element2", "element3"});
-*/
-
-
-		// localpets.add(name, age);
-
-    	localpets.add(new String[] {name, age});
-    	//localpets.add(age);
+    	mypets.add(new String[] {name, age});
     	// printing list of String arrays in the ArrayList
     	// from here: https://stackoverflow.com/questions/3642205/java-arraylist-of-arrays
-        for( int i = 0; i < localpets.size(); i++ ) {
-            for( int j = 0; j < localpets.get(i).length; j++ )
-                System.out.printf(" $ " + localpets.get(i)[j]);
-
-            System.out.println();
-        }
-
-        
-        System.out.println("0808080808080808080");
-        System.out.println(mynewpet);
-        System.out.println("0808080808080808080");
-
-
-    	
-    	//String[] pets = {"Fido", "mangy", "dog", "meowmeow"};
-    	//for (int i = 0; i < pets.length; i++) {
-    	//  System.out.println(pets[i]);
-    //	}
-    	System.out.println("\n\n");
+        //for( int i = 0; i < mypets.size(); i++ ) {
+        //    for( int j = 0; j < mypets.get(i).length; j++ )
+        //        System.out.printf(" $ " + mypets.get(i)[j]);
+        //    System.out.println();
+        //}
+       
+    	System.out.println("\n");
 
     }
 
