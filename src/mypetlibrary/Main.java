c28970cadc3303edc225a1200cff0768e6b737c6
reswanson <swansonr3@csp.edu>
2020-05-14 15:31:32 -0500

    initial checkin

---
+++ src/mypetlibrary/Main.java
@@ -0,0 +1,13 @@
+package mypetlibrary;
+
+public class Main {
+    public static void main(String[] args)
+    {
+     System.out.println("Pet Database Program.\n");
+     
+     
+     PetLibrary ls = new PetLibrary();
+     
+
+    }
+}
---
+++ src/mypetlibrary/PetLibrary.java
@@ -0,0 +1,71 @@
+package mypetlibrary;
+
+import java.io.Console;   
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.Scanner;
+
+
+public class PetLibrary {
+	
+	PetLibrary() {
+	boolean exitnow = false;
+    
+	do {
+			
+		displayMenu();
+		
+        Scanner sc = new Scanner(System.in); 
+     
+        // String input 
+        String choice = sc.nextLine(); 
+		
+ 			
+		switch (choice) {
+		  case "1":
+		    System.out.println("View");
+		    break;
+		  case "2":
+		    System.out.println("add");
+		    break;
+		  case "3":
+		    System.out.println("update");
+		    break;
+		  case "4":
+		    System.out.println("remove");
+		    break;
+		  case "5":
+		    System.out.println("searchbyname");
+		    break;
+		  case "6":
+		    System.out.println("searchbyage");
+		    break;
+		  case "7":
+		    System.out.println("\nGoodbye!");
+		    exitnow=true;
+		    break;
+		}
+	} while (!exitnow);
+	
+		
+	}
+	
+	
+    public final void displayMenu() {
+		System.out.println("What would you like to do?");
+		System.out.println(" 1) View all pets");
+		System.out.println(" 2) Add more pets");
+		System.out.println(" 3) Update an existing pet");
+		System.out.println(" 4) Remove an existing pet");
+		System.out.println(" 5) Search pets by name");
+		System.out.println(" 6) Search pets by age");
+		System.out.println(" 7) Exit program");
+		System.out.println("Your choice: ");
+
+    }
+
+
+}
+
+
